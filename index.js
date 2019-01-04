8f22e70c2b32cc2db2bea95901373cf1d690a075
Zaur Bairamau <bairamau.z@gmail.com>
2019-01-04 16:40:28 +0300

    add reducer

--- index.js
+++ index.js
@@ -1,4 +1,45 @@
-function createStore() {
+// {
+//   type: 'ADD_TODO',
+//   todo: {
+//     id: 0,
+//     name: 'Learn Redux',
+//     complete: false,
+//   }
+// }
+
+// {
+//   type: 'REMOVE_TODO',
+//   id: 0,
+// }
+
+// {
+//   type: 'TOGGLE_TODO',
+//   id: 0,
+// }
+
+// {
+//   type: 'ADD_GOAL',
+//   goal: {
+//     id: 0,
+//     name: 'Run a Marathon'
+//   }
+// }
+
+// {
+//   type: 'REMOVE_GOAL',
+//   id: 0,
+// }
+
+//Reducer
+function todos(state = [], action) {
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo])
+  }
+
+  return state
+}
+
+function createStore(reducer) {
   //The store should have 4 parts
   //1. The state
   //2. Get the state
@@ -17,8 +58,17 @@ function createStore() {
     }
   }
 
+  const dispatch = (action) => {
+    //call todos
+    state = reducer(state, action)
+    //loop over listeners and invoke them
+    listeners.forEach((listener) => listener())
+
+  }
+
   return {
     getState,
     subscribe,
+    dispatch,
   }
 }
