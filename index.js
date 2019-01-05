3ee08770d44d4f4aaec6c699ea8381b7c5ccbfbe
Zaur Bairamau <bairamau.z@gmail.com>
2019-01-05 18:29:48 +0300

    leverage constants

--- index.js
+++ index.js
@@ -1,35 +1,4 @@
-
-//Reducers
-function todos(state = [], action) {
-  if (action.type === 'ADD_TODO') {
-    return state.concat([action.todo])
-  } else if (action.type === 'REMOVE_TODO') {
-    return state.filter((todo) => todo.id !== action.id)
-  } else if (action.type === 'TOGGLE_TODO') {
-    return state.map((todo) => todo.id === action.id ? { ...todo,
-      complete: !todo.complete
-    } : todo)
-  }
-  return state
-}
-
-function goals(state = [], action) {
-  switch (action.type) {
-    case 'ADD_GOAL':
-      return state.concat([action.goal])
-    case 'REMOVE_GOAL':
-      return state.filter((goal) => goal.id !== action.id)
-    default:
-      return state
-  }
-}
-
-function app(state = {}, action){
-  return {
-    todos: todos(state.todos, action),
-    goals: goals(state.goals, action),
-  }
-}
+//Library code
 
 function createStore(reducer) {
   //The store should have 4 parts
@@ -65,13 +34,55 @@ function createStore(reducer) {
   }
 }
 
+
+//App code
+
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
+//Reducers
+function todos(state = [], action) {
+  if (action.type === ADD_TODO) {
+    return state.concat([action.todo])
+  } else if (action.type === REMOVE_TODO) {
+    return state.filter((todo) => todo.id !== action.id)
+  } else if (action.type === TOGGLE_TODO) {
+    return state.map((todo) => todo.id === action.id ? { ...todo,
+      complete: !todo.complete
+    } : todo)
+  }
+  return state
+}
+
+function goals(state = [], action) {
+  switch (action.type) {
+    case ADD_GOAL:
+      return state.concat([action.goal])
+    case REMOVE_GOAL:
+      return state.filter((goal) => goal.id !== action.id)
+    default:
+      return state
+  }
+}
+
+function app(state = {}, action){
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action),
+  }
+}
+
+
 const store = createStore(app)
 const unsubscribe = store.subscribe(() => console.log("The ne state is", store.getState()))
 
 //unsubscribe()
 
 store.dispatch({
-  type: 'ADD_TODO',
+  type: ADD_TODO,
   todo: {
     id: 0,
     name: 'Walk the dog',
@@ -80,7 +91,7 @@ store.dispatch({
 })
 
 store.dispatch({
-  type: 'ADD_TODO',
+  type: ADD_TODO,
   todo: {
     id: 1,
     name: 'Wash the car',
@@ -89,7 +100,7 @@ store.dispatch({
 })
 
 store.dispatch({
-  type: 'ADD_TODO',
+  type: ADD_TODO,
   todo: {
     id: 2,
     name: 'Go to the gym',
@@ -98,17 +109,17 @@ store.dispatch({
 })
 
 store.dispatch({
-  type: 'REMOVE_TODO',
+  type: REMOVE_TODO,
   id: 1
 })
 
 store.dispatch({
-  type: 'TOGGLE_TODO',
+  type: TOGGLE_TODO,
   id: 0
 })
 
 store.dispatch({
-  type: 'ADD_GOAL',
+  type: ADD_GOAL,
   goal: {
     id: 0,
     name: 'Learn Redux'
@@ -116,7 +127,7 @@ store.dispatch({
 })
 
 store.dispatch({
-  type: 'ADD_GOAL',
+  type: ADD_GOAL,
   goal: {
     id: 1,
     name: 'Lose 20 pounds'
@@ -124,6 +135,6 @@ store.dispatch({
 })
 
 store.dispatch({
-  type: 'REMOVE_GOAL',
+  type: REMOVE_GOAL,
   id: 0
 })
