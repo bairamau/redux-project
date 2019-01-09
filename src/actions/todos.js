6cd6fcbe6621713e9d16da2382b7e12bc8373bbe
Zaur Bairamau <bairamau.z@gmail.com>
2019-01-09 23:06:03 +0300

    add actions

---
+++ src/actions/todos.js
@@ -0,0 +1,48 @@
+import API from 'goals-todos-api'
+
+export const ADD_TODO = 'ADD_TODO'
+export const REMOVE_TODO = 'REMOVE_TODO'
+export const TOGGLE_TODO = 'TOGGLE_TODO'
+
+function addTodo(todo) {
+  return {
+    type: ADD_TODO,
+    todo,
+  }
+}
+
+function removeTodo(id) {
+  return {
+    type: REMOVE_TODO,
+    id,
+  }
+}
+
+function toggleTodo(id) {
+  return {
+    type: TOGGLE_TODO,
+    id,
+  }
+}
+
+export function handleAddTodo(name, callback) {
+  return (dispatch) => {
+    return API.saveTodo(name)
+      .then((todo) => {
+        dispatch(addTodo(todo))
+        callback()
+      })
+      .catch(() => alert('an error occured, try again'))
+  }
+}
+
+export function handleDeleteTodo(todo) {
+  return (dispatch) => {
+    dispatch(removeTodo(todo.id))
+    return API.deleteTodo(todo.id)
+      .catch(() => {
+        dispatch(addTodo(todo))
+        alert('an error occured, try again')
+      })
+  }
+}
